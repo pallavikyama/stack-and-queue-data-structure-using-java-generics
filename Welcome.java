a5e4a7054a5d95a39a204f77a5ed33405446854e
pallavikyama <kyamap98@gmail.com>
2020-10-12 13:21:38 +0530

    Added:Welcome message is added.

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package com.blz.stackandqueue;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to creating stack and queue data structure using Java generics.");
+	}
+}
